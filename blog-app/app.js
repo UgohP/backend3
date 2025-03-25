5beab6936a4058142d5b7ccc77f0461610ba9409
Paschal Ugoh <paskyugoh@gmail.com>
2025-03-25 09:15:29 +0100

    I just finished the frontend

--- blog-app/app.js
+++ blog-app/app.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const expressLayout = require("express-ejs-layouts");
 const app = express();
 
 PORT = 4000 || process.env.PORT;
 
-app.use("/wiki/rema", require("./routes/main"));
+app.use(express.static('./public'))
+
+// Template Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/main"));
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
--- blog-app/routes/main.js
+++ blog-app/routes/main.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("", (req, res) => {
-  res.send("Hello Paschal");
+  const locals = {
+    title: 'Nodejs Blog App',
+    description: 'Nodejs Blog App built with Nodejs, Expressjs and MongoDB'
+  }
+  res.render('index', locals);
 });
 
 router.get("/about", (req, res) => {
-    res.send("About Page");
+    res.render("about");
   });
 
 module.exports = router
