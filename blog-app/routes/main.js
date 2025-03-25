7249d9ac14813ba8aa87e2ce5fc97bbb109f8ceb
Paschal Ugoh <paskyugoh@gmail.com>
2025-03-25 12:23:23 +0100

    day 8 of backend class

--- blog-app/routes/main.js
+++ blog-app/routes/main.js
@@ -3,14 +3,10 @@ const router = express.Router();
 
 router.get("", (req, res) => {
   const locals = {
-    title: 'Nodejs Blog App',
-    description: 'Nodejs Blog App built with Nodejs, Expressjs and MongoDB'
-  }
-  res.render('index', locals);
+    title: "Nodejs Blog App",
+    description: "A Nodejs Blog App built with Nodejs, Express and MongoDB",
+  };
+  res.render("index", { locals });
 });
 
-router.get("/about", (req, res) => {
-    res.render("about");
-  });
-
-module.exports = router
+module.exports = router;
