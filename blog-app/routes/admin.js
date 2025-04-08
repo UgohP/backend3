ef80269c81b3ff72f09c300ddec2435d7b919a34
Paschal Ugoh <paskyugoh@gmail.com>
2025-04-08 12:24:01 +0100

    day 14 of the backend3 class. We just finished the register and login page

--- blog-app/routes/admin.js
+++ blog-app/routes/admin.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
 
 router.get("/admin", async (req, res) => {
   try {
@@ -10,6 +14,46 @@ router.get("/admin", async (req, res) => {
       description: "A Nodejs Blog App built with Nodejs, Express and MongoDB",
     };
     res.render("admin/index", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "user created", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "user already in use" });
+      }
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body);
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      res.status(401).json({ message: "User does not exist" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(401).json({ message: "Invalid Password" });
+    }
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/");
   } catch (error) {
     console.log(error);
   }
