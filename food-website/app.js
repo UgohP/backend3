c8116982537f9ef606f8b3404e4d2e7f5c86ff71
Paschal Ugoh <paskyugoh@gmail.com>
2025-04-22 21:50:05 +0100

    started the food website

---
+++ food-website/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const app = express();
+const PORT = 5000;
+
+app.use(express.static("./public"));
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/main"));
+
+app.listen(PORT, () => {
+  console.log(`app is listening on PORT ${PORT}`);
+});
